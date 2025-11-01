6b3464961a8a0175491152f84326b34545506425
Jose <andresjosehr@gmail.com>
2025-11-01 23:03:55 +0100

    Settings

--- config/settings.py
+++ config/settings.py
@@ -28,6 +28,12 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='financial-helper.andresjosehr.com,localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
 
+# CSRF Settings
+CSRF_TRUSTED_ORIGINS = [
+    'https://financial-helper.andresjosehr.com',
+    'http://financial-helper.andresjosehr.com',
+]
+
 
 # Application definition
 
