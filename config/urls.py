27ce525ec684f96d1f6d713858ca217bf5f25372
Andres <Jos Andrs>
2025-11-14 16:46:42 -0400

    CAmbios

--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
 from products.views import get_products_by_categories
+from exchange_rates.views import chart_view
 
-def home(request):
+def api_status(request):
     return JsonResponse({
         'status': 'online',
         'message': 'Financial Helper API is running',
@@ -16,7 +17,8 @@ def home(request):
     })
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', chart_view, name='home'),
+    path('api/status/', api_status, name='api_status'),
     path('admin/', admin.site.urls),
     path('image-processor/', include('image_processor.urls')),
     path('api/', include('image_processor.urls')),
